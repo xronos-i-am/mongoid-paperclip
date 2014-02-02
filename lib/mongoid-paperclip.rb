648c1a649dd1a7815ae9c356ff3dc9e3c287911d
glebtv <glebtv@gmail.com>
2014-02-03 03:12:04 +0400

    monkeypatch rails 4.1 bug for now

--- lib/mongoid-paperclip.rb
+++ lib/mongoid-paperclip.rb
@@ -67,6 +67,36 @@ end
 #  field :avatar_updated_at,   :type => DateTime
 #  field :avatar_fingerprint,  :type => String
 
+require 'paperclip/callbacks'
+module Paperclip
+  module Callbacks
+    module Defining
+      def define_paperclip_callbacks(*callbacks)
+        define_callbacks *[callbacks, {:terminator => callback_terminator}].flatten
+        callbacks.each do |callback|
+          eval <<-end_callbacks
+            def before_#{callback}(*args, &blk)
+              set_callback(:#{callback}, :before, *args, &blk)
+            end
+            def after_#{callback}(*args, &blk)
+              set_callback(:#{callback}, :after, *args, &blk)
+            end
+          end_callbacks
+        end
+      end
+      private
+      def callback_terminator
+        if ::ActiveSupport::VERSION::STRING >= '4.1'
+          lambda { |target, result| result == false }
+        else
+          'result == false'
+        end
+      end
+    end
+  end
+end
+
+
 module Mongoid
   module Paperclip
     extend ActiveSupport::Concern
--- lib/mongoid-paperclip/version.rb
+++ lib/mongoid-paperclip/version.rb
@@ -1,5 +1,5 @@
 module Mongoid
   module Paperclip
-    VERSION = "0.2.1"
+    VERSION = "0.2.2"
   end
 end
